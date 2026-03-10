bbdb96634b826ff7bef44e7bd02e4b8c90e30aa1
chenjie <chenjie.goodday@gmail.com>
2026-03-10 14:08:30 +0800

    feat: git hooks

---
+++ commitlint.config.mjs
@@ -0,0 +1,39 @@
+export default {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'build',
+        'chore',
+        'ci',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'revert',
+        'style',
+        'test',
+        'sample'
+      ]
+    ],
+    'type-case': [2, 'always', ['lower-case']],
+    'scope-case': [2, 'always', ['lower-case']],
+    'scope-enum': [
+      2,
+      'always',
+      [
+        'database',
+        'auth',
+        'utils',
+        'api-contracts',
+        'config',
+        'types',
+        'web',
+        'nest'
+      ]
+    ]
+  }
+}
